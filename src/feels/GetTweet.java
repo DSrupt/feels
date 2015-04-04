63a8bf5686bee6aacee9f25d1940a79c9f9fd90a
Sai Vadlamani <sairvad@gmail.com>
2015-04-04 07:21:55 -0500

    Made GetTweet.java, might need some adjustments

--- src/feels/GetTweet.java
+++ src/feels/GetTweet.java
@@ -1,6 +1,49 @@
 package feels;
 
 public class GetTweet {
-	private String API_Key = "SILZwaTxQ2vmG0OVve2U0qcko";
-	String user,password;
+	
+	private long tweetid = 0L;
+	private TwitterFactory tf;
+	private Twitter twitter;
+	private Status status;
+	public GetTweet() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true);
+		cb.setOAuthConsumerKey("SILZwaTxQ2vmG0OVve2U0qcko");
+		cb.setOAuthConsumerSecret("haT5lwgnhffSF8w0r3ko4QaySu53pSEkUNMowdv3KHyoaYiba7");
+		cb.setOAuthAccessToken("3138262490-oNLCpi1hEu3oAXxSku3Ii0XaQ3cOUIXVhVRgU8V");
+		cb.setOAuthAccessTokenSecret("pYAvBtCR9tnDE2Ru3t8fRIIso6HzU3ywOfl29dJWkYZG1");
+		tf = new TwitterFactory(cb.build());
+		twitter = tf.getInstance();
+	}
+	
+	public GetTweet(long id) {
+		tweetid = id;
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true);
+		cb.setOAuthConsumerKey("SILZwaTxQ2vmG0OVve2U0qcko");
+		cb.setOAuthConsumerSecret("haT5lwgnhffSF8w0r3ko4QaySu53pSEkUNMowdv3KHyoaYiba7");
+		cb.setOAuthAccessToken("3138262490-oNLCpi1hEu3oAXxSku3Ii0XaQ3cOUIXVhVRgU8V");
+		cb.setOAuthAccessTokenSecret("pYAvBtCR9tnDE2Ru3t8fRIIso6HzU3ywOfl29dJWkYZG1");
+		tf = new TwitterFactory(cb.build());
+		twitter = tf.getInstance();
+		status = twitter.showStatus(tweetid);
+	}
+	
+	public long getID() {
+		return tweetid;
+	}
+	
+	public void setID(long id) {
+		tweetid = id;
+	}
+	
+	public String returnTweetMessage() {
+		return status.getText();
+	}
+	
+	public String getGeo() {
+		return "" + status.getGeoLocation();
+	}
+	
 }
