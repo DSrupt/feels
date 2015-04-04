853674fed8d25e53e1cd5f37580c514f28f6945a
Sai Vadlamani <sairvad@gmail.com>
2015-04-04 07:25:54 -0500

    Added imports

--- src/feels/GetTweet.java
+++ src/feels/GetTweet.java
@@ -1,5 +1,13 @@
 package feels;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class GetTweet {
 	
 	private long tweetid = 0L;
